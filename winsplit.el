67c08fcab0ff0425f4663fd5334bc11bae94acf8
Dave Pearson <davep@davep.org>
2017-04-10 13:39:40 +0100

    Add commands to split and then prompt to load a file

--- winsplit.el
+++ winsplit.el
@@ -19,22 +19,54 @@
   (interactive)
   (split-window-horizontally))
 
+(defun winsplit-left-load ()
+  "Create a new focused window to the left of the current window.
+
+Also prompt for a file to visit."
+  (interactive)
+  (winsplit-left)
+  (call-interactively #'find-file))
+
 (defun winsplit-right ()
   "Create a new focused window to the right of the current window."
   (interactive)
   (winsplit-left)
   (other-window 1))
 
+(defun winsplit-right-load ()
+  "Create a new focused window to the right of the current window..
+
+Also prompt for a file to visit."
+  (interactive)
+  (winsplit-right)
+  (call-interactively #'find-file))
+
 (defun winsplit-above ()
   "Create a new focused window above the current window."
   (interactive)
   (split-window-vertically))
+
+(defun winsplit-above-load ()
+  "Create a new focused window above the current window..
+
+Also prompt for a file to visit."
+  (interactive)
+  (winsplit-right)
+  (call-interactively #'find-file))
 
 (defun winsplit-below ()
   "Create a new focused window below the current window."
   (interactive)
   (winsplit-above)
   (other-window 1))
+
+(defun winsplit-below-load ()
+  "Create a new focused window below the current window..
+
+Also prompt for a file to visit."
+  (interactive)
+  (winsplit-below)
+  (call-interactively #'find-file))
 
 (provide 'winsplit)
 
